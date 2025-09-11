dca937e6f3c4329ab9ab8fcef873d252c92a3662
gabriel <gabrielmiguel1309@outlook.com>
2025-09-11 13:18:10 -0300

    creation of the book dto

---
+++ src/main/java/com/gabriel_f_s/bookstore/mapper/dtos/BookDTO.java
@@ -0,0 +1,20 @@
+package com.gabriel_f_s.bookstore.mapper.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDTO implements Serializable {
+
+    private Long id;
+    private String title;
+    private String isbn;
+}
