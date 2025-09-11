d1f53c8d6bfc0e6149f0bb815216b7de59df4013
gabriel <gabrielmiguel1309@outlook.com>
2025-09-11 13:24:46 -0300

    change in the test configuration class

--- src/main/java/com/gabriel_f_s/bookstore/configs/TestConfiguration.java
+++ src/main/java/com/gabriel_f_s/bookstore/configs/TestConfiguration.java
@@ -1,6 +1,8 @@
 package com.gabriel_f_s.bookstore.configs;
 
+import com.gabriel_f_s.bookstore.entities.Author;
 import com.gabriel_f_s.bookstore.entities.Book;
+import com.gabriel_f_s.bookstore.repositories.AuthorRepository;
 import com.gabriel_f_s.bookstore.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,22 +10,32 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 @Configuration
 public class TestConfiguration implements CommandLineRunner {
 
     @Autowired
     private BookRepository bookRepository;
+    @Autowired
+    private AuthorRepository authorRepository;
 
     @Override
     public void run(String... args) throws Exception {
-        List<Book> books = Arrays.asList(
-                new Book(null, "Código Limpo: Habilidades Práticas do Agile Software", 75.99, 425),
-                new Book(null, "Arquitetura Limpa: o Guia do Artesão Para Estrutura e Design de Software", 78.99, 432),
-                new Book(null, "Domain-Driven Design: Atacando as Complexidades no Coração do Software", 102.75, 528),
-                new Book(null, "Entendendo Algoritmos", 54.15, 264)
+        List<Author> authors = Arrays.asList(
+                new Author(null, "J.K. Rowling"),
+                new Author(null, "Robert C. Martin"),
+                new Author(null, "Eric Evans")
         );
+        authorRepository.saveAll(authors);
 
+
+        List<Book> books = Arrays.asList(
+                new Book(null, "Harry Potter e a Pedra Filosofal", "978-8532530783", Set.of(authors.get(0))),
+                new Book(null, "Código Limpo: Habilidades Práticas do Agile Software", "978-8576082675", Set.of(authors.get(1))),
+                new Book(null, "Arquitetura Limpa: o Guia do Artesão Para Estrutura e Design de Software", "978-8550804606", Set.of(authors.get(1))),
+                new Book(null, "Domain-Driven Design: Atacando as Complexidades no Coração do Software", "978-8550800653", Set.of(authors.get(2)))
+        );
         bookRepository.saveAll(books);
     }
 }
