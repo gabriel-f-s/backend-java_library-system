cee5fd2c4bb8b8f30e45bfe477032be770d2bb2c
gabriel <gabrielmiguel1309@outlook.com>
2025-09-11 13:24:21 -0300

    created the dto that represents the relationships between books

---
+++ src/main/java/com/gabriel_f_s/bookstore/mapper/dtos/BooksWithRelationsDTO.java
@@ -0,0 +1,19 @@
+package com.gabriel_f_s.bookstore.mapper.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BooksWithRelationsDTO {
+
+    private Long id;
+    private String title;
+    private String isbn;
+    private Set<AuthorDTO> authors;
+
+}
